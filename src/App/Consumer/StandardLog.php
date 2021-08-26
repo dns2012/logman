25131c44f3e9c3afa41fb1a770a19400e307845f
dns2012 <dnsprogress@gmail.com>
2021-08-26 08:17:01 +0700

    add php non fatal error and exception handler

--- src/App/Consumer/StandardLog.php
+++ src/App/Consumer/StandardLog.php
@@ -20,7 +20,9 @@ class StandardLog extends ConsumerAbstract
     {
         $this->logDTO = $logDTO;
 
-        register_shutdown_function(array($this, 'errorHandler'));
+        register_shutdown_function([$this, 'fatalErrorHandler']);
+        set_error_handler([$this, 'errorHandler']);
+        set_exception_handler([$this, 'exceptionHandler']);
         ini_set('display_errors', false);
         ini_set('log_errors', TRUE);
         ini_set('error_reporting', E_ALL);
@@ -29,7 +31,7 @@ class StandardLog extends ConsumerAbstract
     /**
      * @return void
      */
-    public function errorHandler()
+    public function fatalErrorHandler()
     {
         $lastError = error_get_last();
         if (! empty($lastError)) {
@@ -42,19 +44,7 @@ class StandardLog extends ConsumerAbstract
                     break;
                 case E_ERROR:
                 case E_PARSE:
-                case E_USER_ERROR:
-                case E_RECOVERABLE_ERROR:
                     $errorLevel = LogLevel::ERROR;
-                    break;
-                case E_WARNING:
-                case E_NOTICE:
-                case E_USER_WARNING:
-                case E_USER_NOTICE:
-                    $errorLevel = LogLevel::WARNING;
-                    break;
-                case E_DEPRECATED:
-                case E_USER_DEPRECATED:
-                    $errorLevel = LogLevel::INFO;
                     break;
                 default:
                     $errorLevel = LogLevel::ERROR;
@@ -66,12 +56,61 @@ class StandardLog extends ConsumerAbstract
     }
 
     /**
+     * @param  int $errorLevel
+     * @param  string $errorMessage
+     * @param  string $errorFile
+     * @param  int $errorLine
+     * @return boolean
+     */
+    public function errorHandler($errorLevel, $errorMessage, $errorFile, $errorLine)
+    {
+        if (! (error_reporting() & $errorLevel)) {
+            return false;
+        }
+
+        switch ($errorLevel) {
+            case E_USER_ERROR:
+            case E_RECOVERABLE_ERROR:
+                $errorLevel = LogLevel::ERROR;
+            case E_WARNING:
+            case E_NOTICE:
+            case E_USER_WARNING:
+            case E_USER_NOTICE:
+                $errorLevel = LogLevel::WARNING;
+                break;
+            case E_DEPRECATED:
+            case E_USER_DEPRECATED:
+                $errorLevel = LogLevel::INFO;
+                break;
+            default:
+                $errorLevel = LogLevel::ERROR;
+                break;
+        }
+
+        $message = preg_replace('/\r|\n/', '', $errorMessage) . " | FILE : " . $errorFile . " | LINE : " . $errorLine;
+        $this->invoker($errorLevel, [$this->logDTO, $message]);
+        return true;
+    }
+
+    /**
+     * @param  object $exception
+     * @return void
+     */
+    public function exceptionHandler($exception)
+    {
+        $message = preg_replace('/\r|\n/', '', $exception->getMessage()) . " | FILE : " . $exception->getFile() . " | LINE : " . $exception->getLine();
+        $this->invoker(LogLevel::ERROR, [$this->logDTO, $message, ['type' => 'Throw Exception']]);
+    }
+
+    /**
      * @param  string $methodName
      * @param  array $args
      * @return void
      */
-    protected function invoker($methodName = NULL, $args = []){
-        $this->{$methodName}($args[0], $args[1]);
+    protected function invoker($methodName = NULL, $args = [])
+    {
+        $context = isset($args[2]) ? $args[2] : [];
+        $this->{$methodName}($args[0], $args[1], $context);
     }
 
     /**
